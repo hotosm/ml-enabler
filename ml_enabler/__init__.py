480fee064e1b5aecd87c02b8cf4a9a93a0d49f20
Sajjad Anwar <sajjadkm@gmail.com>
2019-05-22 15:32:13 +0530

    / for healthcheck

--- ml_enabler/__init__.py
+++ ml_enabler/__init__.py
@@ -30,7 +30,7 @@ def init_routes(app):
     # import apis
     from ml_enabler.api.ml import StatusCheckAPI
 
-    api.add_resource(StatusCheckAPI, '/api/status')
+    api.add_resource(StatusCheckAPI, '/')
 
 
 if __name__ == '__main__':
