d9fff9b0af727f6bbb2a73455de2e420310fa221
Sajjad Anwar <sajjadkm@gmail.com>
2019-06-20 13:41:30 +0530

    version namespace routes

--- ml_enabler/tests/test_api.py
+++ ml_enabler/tests/test_api.py
@@ -19,7 +19,7 @@ class PredictionAPITest(BaseTestCase):
             "bbox": [10.013795, 53.5225, 10.048885, 53.540843],
             "tileZoom": 18
         }
-        response = self.client.post('model/1/prediction',
+        response = self.client.post('/v1/model/1/prediction',
                                     data=json.dumps(prediction_payload),
                                     content_type='application/json')
         assert(response.get_json() == {'error': 'model not found'})
@@ -37,7 +37,7 @@ class PredictionAPITest(BaseTestCase):
             "bbox": [10.013795, 53.5225, 10.048885, 53.540843],
             "tileZoom": 18
         }
-        response = self.client.post(f'model/{ml_model.id}/prediction',
+        response = self.client.post(f'/v1/model/{ml_model.id}/prediction',
                                     data=json.dumps(prediction_payload),
                                     content_type='application/json')
         assert(response.get_json() == {'prediction_id': ml_model.id})
@@ -47,7 +47,7 @@ class PredictionAPITest(BaseTestCase):
         prediction = create_prediction()
         ml_model_id = prediction.model_id
 
-        response = self.client.get(f'model/{ml_model_id}/prediction?'
+        response = self.client.get(f'/v1/model/{ml_model_id}/prediction?'
                                    'bbox=10.013795,53.5225,'
                                    '10.048885,53.540843')
         assert(len(response.get_json()) == 1)
@@ -59,7 +59,7 @@ class PredictionTileAPITest(BaseTestCase):
         prediction = create_prediction()
         payload = tiles.tiles_for_prediction(prediction.id)
 
-        response = self.client.post(f'model/prediction/{prediction.id}/tiles',
+        response = self.client.post(f'/v1/model/prediction/{prediction.id}/tiles',
                                     data=json.dumps(payload),
                                     content_type='application/json')
         assert(response.status_code == 200)
@@ -71,7 +71,7 @@ class PredictionTileAPITest(BaseTestCase):
 
         payload = geojson.get_geojson()
 
-        response = self.client.post(f'model/{prediction.model_id}/tiles/geojson',
+        response = self.client.post(f'/v1/model/{prediction.model_id}/tiles/geojson',
                                     data=json.dumps(payload),
                                     content_type='application/json')
 
@@ -82,13 +82,13 @@ class PredictionTileAPITest(BaseTestCase):
 
         prediction = create_prediction()
         create_prediction_tiles(prediction.id)
-        response = self.client.get(f'model/{prediction.model_id}/tiles?bbox=10.013795,53.5225,10.048885,53.540843&zoom=18')
+        response = self.client.get(f'/v1/model/{prediction.model_id}/tiles?bbox=10.013795,53.5225,10.048885,53.540843&zoom=18')
         assert(response.status_code == 200)
 
     def test_get_outside_bbox(self):
 
         prediction = create_prediction()
         create_prediction_tiles(prediction.id)
-        response = self.client.get(f'model/{prediction.model_id}/tiles?bbox=10.05,53.52,10.08,53.54&zoom=18')
+        response = self.client.get(f'/v1/model/{prediction.model_id}/tiles?bbox=10.05,53.52,10.08,53.54&zoom=18')
         assert(response.get_json() == {'error': 'No predictions for this bbox'})
         assert(response.status_code == 404)
