b01af853585caafaee4d2ef3fa9940524a85be5a
ingalls <nick@ingalls.ca>
2020-02-26 09:34:32 -0800

    Add Model component

--- web/src/App.vue
+++ web/src/App.vue
@@ -49,36 +49,7 @@
                 <EditModel :model='JSON.parse(JSON.stringify(model))' v-on:close='getModels'/>
             </template>
             <template v-else-if='mode === "model"'>
-                <div class='col col--12 clearfix py6'>
-                    <h2 class='fl cursor-default' v-text='model.name + " - " + model.source'></h2>
-
-                    <button @click='mode = "models"' class='btn fr round btn--stroke color-gray color-black-on-hover'>
-                        <svg class='icon'><use href='#icon-close'/></svg>
-                    </button>
-
-                    <button @click='external(model.projectUrl)' class='mr12 btn fr round btn--stroke color-gray color-black-on-hover'>
-                        <svg class='icon'><use href='#icon-link'/></svg>
-                    </button>
-
-                    <button @click='editModel(model.modelId)' class='mr12 btn fr round btn--stroke color-gray color-black-on-hover'>
-                        <svg class='icon'><use href='#icon-pencil'/></svg>
-                    </button>
-                </div>
-                <div class='border border--gray-light round col col--12 px12 py12 clearfix'>
-                    <div class='grid grid--gut12'>
-                        <div class='col col--12 py6'>
-                            <template v-if='models.length === 0'>
-                                <div class='flex-parent flex-parent--center-main pt36'>
-                                    <svg class='flex-child icon w60 h60 color--gray'><use href='#icon-info'/></svg>
-                                </div>
-
-                                <div class='flex-parent flex-parent--center-main pt12 pb36'>
-                                    <h1 class='flex-child txt-h4 cursor-default'>Create a model to get started!</h1>
-                                </div>
-                            </template>
-                        </div>
-                    </div>
-                </div>
+                <Model :model='JSON.parse(JSON.stringify(model))' v-on:close='getModels'/>
             </template>
         </div>
     </div>
@@ -86,6 +57,7 @@
 
 <script>
 
+import Model from './components/Model.vue';
 import EditModel from './components/EditModel.vue';
 
 export default {
@@ -103,6 +75,7 @@ export default {
         }
     },
     components: {
+        Model,
         EditModel
     },
     watch: {
---
+++ web/src/components/Model.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="col col--12">
+        <div class='col col--12 clearfix py6'>
+            <h2 class='fl cursor-default' v-text='model.name + " - " + model.source'></h2>
+
+            <button @click='mode = "models"' class='btn fr round btn--stroke color-gray color-black-on-hover'>
+                <svg class='icon'><use href='#icon-close'/></svg>
+            </button>
+
+            <button @click='external(model.projectUrl)' class='mr12 btn fr round btn--stroke color-gray color-black-on-hover'>
+                <svg class='icon'><use href='#icon-link'/></svg>
+            </button>
+
+            <button @click='editModel(model.modelId)' class='mr12 btn fr round btn--stroke color-gray color-black-on-hover'>
+                <svg class='icon'><use href='#icon-pencil'/></svg>
+            </button>
+        </div>
+        <div class='border border--gray-light round col col--12 px12 py12 clearfix'>
+            <div class='grid grid--gut12'>
+                <div class='col col--12 py6'>
+                    <template v-if='models.length === 0'>
+                        <div class='flex-parent flex-parent--center-main pt36'>
+                            <svg class='flex-child icon w60 h60 color--gray'><use href='#icon-info'/></svg>
+                        </div>
+
+                        <div class='flex-parent flex-parent--center-main pt12 pb36'>
+                            <h1 class='flex-child txt-h4 cursor-default'>Create a model to get started!</h1>
+                        </div>
+                    </template>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Model',
+    props: ['model'],
+    methods: {
+        close: function() {
+            this.$emit('close');
+        },
+    }
+}
+</script>
