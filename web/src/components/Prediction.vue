c444dacb61c8d6cde0b64c6f2e864a067bb898a7
ingalls <nick@ingalls.ca>
2020-03-06 11:08:54 -0800

    Add log & ECR links

--- web/src/components/Prediction.vue
+++ web/src/components/Prediction.vue
@@ -11,19 +11,18 @@
             <template v-if='prediction.modelLink'>
                 <div class='col col--12 border-b border--gray-light clearfix mb6'>
                     <h3 class='fl mt6 cursor-default'>Assets:</h3>
+
+                    <button v-if='prediction.logLink' class='fr btn btn--s btn--stroke color-gray color-blue-on-hover round ml6'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg>Build Log</button>
+                    <button v-if='prediction.dockerLink' class='fr btn btn--s btn--stroke color-gray color-blue-on-hover round mx6'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg> ECR</button>
                 </div>
 
                 <div class='col col--12 py3'>
-                    <button class='btn btn--stroke round mx6'><svg class='icon'><use href='#icon-arrow-down'/></svg></button> Raw TF compatible Model
+                    <div class='align-center'>TF Model</div>
+                    <pre class='pre' v-text='"s3://" + prediction.modelLink'></pre>
                 </div>
                 <div v-if='prediction.saveLink' class='col col--12 py3'>
-                    <button class='btn btn--stroke round mx6'><svg class='icon'><use href='#icon-arrow-down'/></svg></button> TFServing Docker Image
-                </div>
-                <div v-if='prediction.logLink' class='col col--12 py3'>
-                    <button class='btn btn--stroke round mx6'><svg class='icon'><use href='#icon-link'/></svg></button>Link to build log
-                </div>
-                <div v-if='prediction.dockerLink' class='col col--12 py3'>
-                    <button class='btn btn--stroke round mx6'><svg class='icon'><use href='#icon-link'/></svg></button>ECR Docker Image
+                    <div class='align-center'>TFServing Container</div>
+                    <pre class='pre' v-text='"s3://" + prediction.saveLink'></pre>
                 </div>
             </template>
             <template v-else>
