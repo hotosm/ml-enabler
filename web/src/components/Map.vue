f4829da3295ff562e3afe742081847b36c24c4bc
ingalls <nick@ingalls.ca>
2020-03-13 11:23:39 -0700

    Wire up threshold & opacity sliders

--- web/src/components/Map.vue
+++ web/src/components/Map.vue
@@ -16,6 +16,33 @@
                 <div class='col col--12 clearfix pt6'>
                     <button @click='bboxzoom' class='btn round btn--stroke fl btn--gray'><svg class='icon'><use xlink:href='#icon-viewport'/></svg></button>
                 </div>
+
+                <template v-if='!advanced'>
+                    <div class='col col--12'>
+                        <button @click='advanced = !advanced' class='btn btn--white color-gray px0'><svg class='icon fl my6'><use xlink:href='#icon-chevron-right'/></svg><span class='fl pl6'>Advanced Options</span></button>
+                    </div>
+                </template>
+                <template v-else>
+                    <div class='col col--12 border-b border--gray-light mb12'>
+                        <button @click='advanced = !advanced' class='btn btn--white color-gray px0'><svg class='icon fl my6'><use xlink:href='#icon-chevron-down'/></svg><span class='fl pl6'>Advanced Options</span></button>
+                    </div>
+                </template>
+
+                <template v-if='advanced'>
+                    <div class='col col--12'>
+                        <label>Opacity (<span v-text='opacity'/>%)</label>
+                        <div class='range range--s color-gray'>
+                            <input v-on:input='opacity = parseInt($event.target.value)' type='range' min=0 max=100 />
+                        </div>
+                    </div>
+
+                    <div class='col col--12'>
+                        <label>Threshold (<span v-text='threshold'/>%)</label>
+                        <div class='range range--s color-gray'>
+                            <input v-on:input='threshold = parseInt($event.target.value)' type='range' min=0 max=100 />
+                        </div>
+                    </div>
+                </template>
             </div>
 
             <div class='bg-white round absolute top right z5 mx12 my12'>
@@ -33,7 +60,7 @@
                     </div>
                     <div class='flex-parent flex-parent--center-main'>
                         <div class='flex-child'>
-                            <span v-text='this.layer'></span>: <span v-text='(this.inspect * 100).toFixed(1)'></span>%
+                            <span v-text='this.inf'></span>: <span v-text='(this.inspect * 100).toFixed(1)'></span>%
                         </div>
                     </div>
                 </template>
@@ -67,11 +94,27 @@ export default {
         return {
             inf: false,
             inspect: false,
-            opacity: 0.5,
+            advanced: false,
+            threshold: 50,
+            opacity: 50,
             map: false
         };
     },
     watch: {
+        opacity: function() {
+            for (const inf of this.tilejson.inferences) {
+                this.map.setPaintProperty(
+                    `inf-${inf}`,
+                    'fill-opacity',
+                    [ 'number', [ '*', ['get', inf], (this.opacity / 100) ] ]
+                );
+            }
+        },
+        threshold: function() {
+            for (const inf of this.tilejson.inferences) {
+                this.filter(inf);
+            }
+        },
         inf: function() {
             for (const inf of this.tilejson.inferences) {
                 this.map.setLayoutProperty(`inf-${inf}`, 'visibility', 'none');
@@ -144,9 +187,27 @@ export default {
                             'fill-color': '#ff0000',
                             'fill-opacity': [
                                 'number',
-                                [ '*', ['get', inf], this.opacity ]
+                                [ '*', ['get', inf], (this.opacity / 100) ]
                             ]
                         }
+                    });
+
+                    this.filter(inf);
+
+                    this.map.on('mousemove', `inf-${inf}`, (e) => {
+                        if (
+                            e.features.length === 0
+                            || !e.features[0].properties[this.inf]
+                            || e.features[0].properties[this.inf] === 0
+                        ) {
+                            this.map.getCanvas().style.cursor = '';
+                            this.inspect = false;
+                            return;
+                        }
+
+                        this.map.getCanvas().style.cursor = 'pointer';
+
+                        this.inspect = e.features[0].properties[this.inf];
                     });
                 }
 
@@ -158,6 +219,9 @@ export default {
                 [this.tilejson.bounds[0], this.tilejson.bounds[1]],
                 [this.tilejson.bounds[2], this.tilejson.bounds[3]]
             ]);
+        },
+        filter: function(inf) {
+            this.map.setFilter(`inf-${inf}`, ['>=', inf, this.threshold / 100]);
         },
         fullscreen: function() {
             const container = document.querySelector('#map-container');
