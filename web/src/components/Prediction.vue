dc3084331b8808f5ead9b646f12402d4819d364d
ingalls <nick@ingalls.ca>
2020-03-24 09:19:09 -0700

    Add stack component

--- web/src/components/Prediction.vue
+++ web/src/components/Prediction.vue
@@ -10,37 +10,73 @@
             <span class='fr mr6 bg-blue-faint bg-blue-on-hover color-white-on-hover color-blue inline-block px6 py3 round txt-xs txt-bold cursor-pointer' v-text='"id: " + prediction.predictionsId'/>
         </div>
         <div class='border border--gray-light round col col--12 px12 py12 clearfix'>
-            <template v-if='prediction.modelLink'>
-                <div class='col col--12 border-b border--gray-light clearfix mb6'>
-                    <h3 class='fl mt6 cursor-default'>Assets:</h3>
-
-                    <button v-if='prediction.logLink' @click='logLink(prediction.logLink)' class='fr btn btn--s btn--stroke color-gray color-blue-on-hover round ml6'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg>Build Log</button>
-                    <button v-if='prediction.dockerLink' @click='ecrLink(prediction.dockerLink)' class='fr btn btn--s btn--stroke color-gray color-blue-on-hover round mx6'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg> ECR</button>
+            <div class='col col--12 border-b border--gray-light clearfix mb6'>
+                <div class="flex-parent-inline py3">
+                    <button @click='mode = "assets"' class="btn btn--pill btn--s btn--pill-hl round">Assets</button>
+                    <button @click='mode = "stack"' class="btn btn--pill btn--s btn--pill-hc round">Stack</button>
+                    <button @click='mode = "map"' class="btn btn--pill btn--s btn--pill-hr round">Map</button>
                 </div>
 
-                <div class='col col--12 py3'>
-                    <div class='align-center'>TF Model</div>
-                    <pre class='pre' v-text='"s3://" + prediction.modelLink'></pre>
+                <div v-if='mode === "assets"' class="flex-parent-inline py3 fr">
+                    <button v-if='prediction.logLink' @click='logLink(prediction.logLink)' class='mr6 btn btn--s btn--stroke color-gray color-blue-on-hover round'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg>Build Log</button>
+                    <button v-if='prediction.dockerLink' @click='ecrLink(prediction.dockerLink)' class='btn btn--s btn--stroke color-gray color-blue-on-hover round'><svg class='icon fl' style='margin-top: 4px;'><use href='#icon-link'/></svg> ECR</button>
                 </div>
-                <div v-if='prediction.saveLink' class='col col--12 py3'>
-                    <div class='align-center'>TFServing Container</div>
-                    <pre class='pre' v-text='"s3://" + prediction.saveLink'></pre>
-                </div>
-                <div v-if='prediction.saveLink' class='col col--12 py3'>
-                    <div class='align-center'>ECR Container</div>
-                    <pre class='pre' v-text='prediction.dockerLink'></pre>
-                </div>
+            </div>
+
+            <template v-if='mode === "assets"'>
+                <template v-if='prediction.modelLink'>
+                    <div class='col col--12 py3'>
+                        <div class='align-center'>TF Model</div>
+                        <pre class='pre' v-text='"s3://" + prediction.modelLink'></pre>
+                    </div>
+                    <div v-if='prediction.saveLink' class='col col--12 py3'>
+                        <div class='align-center'>TFServing Container</div>
+                        <pre class='pre' v-text='"s3://" + prediction.saveLink'></pre>
+                    </div>
+                    <div v-if='prediction.saveLink' class='col col--12 py3'>
+                        <div class='align-center'>ECR Container</div>
+                        <pre class='pre' v-text='prediction.dockerLink'></pre>
+                    </div>
+                </template>
+                <template v-else>
+                        <div class='align-center pb6'>Upload a model to get started</div>
+
+                        <UploadPrediction :prediction='prediction' v-on:close='close'/>
+                </template>
             </template>
-            <template v-else>
-                <div class='align-center pb6'>Upload a model to get started</div>
+            <template v-else-if='mode === "stack"'>
+                <template v-if='!prediction.modelLink'>
+                    <div class='col col--12 py6'>
+                        <div class='flex-parent flex-parent--center-main pt36'>
+                            <svg class='flex-child icon w60 h60 color--gray'><use href='#icon-info'/></svg>
+                        </div>
 
-                <UploadPrediction :prediction='prediction' v-on:close='close'/>
+                        <div class='flex-parent flex-parent--center-main pt12 pb36'>
+                            <h1 class='flex-child txt-h4 cursor-default'>A Model must be uploaded before a stack is created</h1>
+                        </div>
+                    </div>
+                </template>
+                <template v-else>
+                    <Stack :prediction='prediction'/>
+                </template>
             </template>
+            <template v-else-if='mode === "map"'>
+                <template v-if='tiles'>
+                    <div class='align-center pb6'>Prediction Tiles</div>
 
-            <template v-if='tiles'>
-                <div class='align-center pb6'>Prediction Tiles</div>
+                    <Map :prediction='prediction' :tilejson='tiles'/>
+                </template>
+                <template v-else>
+                    <div class='col col--12 py6'>
+                        <div class='flex-parent flex-parent--center-main pt36'>
+                            <svg class='flex-child icon w60 h60 color-gray'><use href='#icon-info'/></svg>
+                        </div>
 
-                <Map :prediction='prediction' :tilejson='tiles'/>
+                        <div class='flex-parent flex-parent--center-main pt12 pb36'>
+                            <h1 class='flex-child txt-h4 cursor-default'>No Inferences Uploaded</h1>
+                        </div>
+                    </div>
+                </template>
             </template>
         </div>
     </div>
@@ -48,6 +84,7 @@
 
 <script>
 import UploadPrediction from './UploadPrediction.vue';
+import Stack from './Stack.vue';
 import Map from './Map.vue';
 
 export default {
@@ -55,7 +92,7 @@ export default {
     props: ['model', 'prediction'],
     data: function() {
         return {
-            mode: 'prediction',
+            mode: 'assets',
             tiles: false
         }
     },
@@ -64,6 +101,7 @@ export default {
     },
     components: {
         Map,
+        Stack,
         UploadPrediction
     },
     methods: {
---
+++ web/src/components/Stack.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='col col--12'>
+        <template v-if='!stack'>
+            <h2 class='w-full align-center txt-h4 py12'>No Stack Deployed</h2>
+            <div class='flex-parent flex-parent--center-main py12'>
+                <button class='flex-child btn btn--stroke round'>Create Stack</button>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Stack',
+    props: ['prediction'],
+    data: function() {
+        return {
+            loading: true,
+            stack: false
+        };
+    },
+    methods: {
+        getStatus() {
+            this.loading = false;
+        }
+    }
+}
+</script>
