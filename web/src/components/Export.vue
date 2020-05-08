397882139832d00a60ef4181bfbf81e3236e3a99
ingalls <nick@ingalls.ca>
2020-05-08 11:10:47 -0700

    Add api prefix to export

--- web/src/components/Export.vue
+++ web/src/components/Export.vue
@@ -81,7 +81,7 @@ export default {
     },
     methods: {
         getExport: function() {
-            const url = new URL(`${window.location.origin}/v1/model/${this.model.modelId}/prediction/${this.prediction.predictionsId}/export`);
+            const url = new URL(`${window.location.origin}${window.api}/v1/model/${this.model.modelId}/prediction/${this.prediction.predictionsId}/export`);
 
             url.searchParams.set('format', this.params.format);
             url.searchParams.set('inferences', this.params.inferences);
