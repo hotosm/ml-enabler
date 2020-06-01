f5550cbd43b53c2eaf8fd59e1e7a2cba2cfa8406
ingalls <nick@ingalls.ca>
2020-06-01 12:41:19 -0700

    Remove functions now included with alchemy

--- ml_enabler/models/utils.py
+++ ml_enabler/models/utils.py
@@ -1,7 +1,4 @@
 import datetime
-from geoalchemy2 import Geometry
-from geoalchemy2.functions import GenericFunction
-
 
 class NotFound(Exception):
     """ Custom exception to indicate model not found in database"""
@@ -25,26 +22,3 @@ def timestamp():
     timestamp when new models initialised"""
     return datetime.datetime.utcnow()
 
-
-class ST_GeomFromText(GenericFunction):
-    """ Export the postgis ST_GeomFromText function """
-    name = 'ST_GeomFromText'
-    type = Geometry
-
-
-class ST_Intersects(GenericFunction):
-    """ Exposes PostGIS ST_Intersects function """
-    name = 'ST_Intersects'
-    type = Geometry
-
-
-class ST_MakeEnvelope(GenericFunction):
-    """ Exposes PostGIS ST_MakeEnvelope function """
-    name = 'ST_MakeEnvelope'
-    type = Geometry
-
-
-class ST_AsText(GenericFunction):
-    """ Exposes PostGIS ST_AsText function """
-    name = 'ST_AsText'
-    type = Geometry
